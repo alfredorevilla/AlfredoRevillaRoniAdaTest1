7e2cf9a0cb3c9f08f28b90f095e5067a1776ccbb
Alfredo Revilla <alfredorevilla@gmail.com>
2020-08-12 20:43:38 -0500

    error handling.

---
+++ src/AlfredoRevillaRoniAdaTest1/Controllers/ErrorController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlfredoRevillaRoniAdaTest1.Controllers
+{
+    [ApiController]
+    public class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult Error([FromServices] IWebHostEnvironment webHostEnvironment)
+        {
+            var error = HttpContext.Features.Get<IExceptionHandlerFeature>().Error;
+
+            while (error.InnerException != null)
+            {
+                error = error.InnerException;
+            }
+
+            return Problem(
+                title: error.Message,
+#if DEBUG
+                detail: error.StackTrace
+#endif
+                );
+
+        }
+    }
+}
--- src/AlfredoRevillaRoniAdaTest1/Startup.cs
+++ src/AlfredoRevillaRoniAdaTest1/Startup.cs
@@ -40,10 +40,7 @@ namespace AlfredoRevillaRoniAdaTest1
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseExceptionHandler("/error");
 
             app.UseHttpsRedirection();
 
